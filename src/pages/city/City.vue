aea2957a8543e019fa74212dc0636bffb2a37cdd
jiangyx3915 <jyx15221613915@gmail.com>
2018-04-22 17:07:12 +0800

    完成城市列表选择

---
+++ src/pages/city/City.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <city-header></city-header>
+    <city-search :cities="cities"></city-search>
+    <city-list :hotCities="hotCities" :cities="cities" :letter="letter"></city-list>
+    <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
+export default {
+  name: 'City',
+  components: {
+    CityHeader,
+    CitySearch,
+    CityList,
+    CityAlphabet
+  },
+  data () {
+    return {
+      hotCities: [],
+      cities: {},
+      letter: ''
+    }
+  },
+  mounted () {
+    this.getCityInfo()
+  },
+  methods: {
+    async getCityInfo () {
+      let res = await axios.get('/api/city.json')
+      let data = res.data
+      if (data.ret && data.data) {
+        data = data.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
