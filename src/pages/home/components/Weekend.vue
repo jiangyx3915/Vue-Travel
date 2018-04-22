bc67224b3b879f9530ab4faadc8a3f6bdd3f0225
jiangyx3915 <jyx15221613915@gmail.com>
2018-04-22 13:19:37 +0800

    完成周末推荐部分组件

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="title">周末推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
+          title: '大连海洋世界',
+          desc: '大连海洋世界大连海洋世界大连海洋世界'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
+          title: '大连海洋世界',
+          desc: '大连海洋世界大连海洋世界大连海洋世界'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
+          title: '大连海洋世界',
+          desc: '大连海洋世界大连海洋世界大连海洋世界'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 37.09%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
